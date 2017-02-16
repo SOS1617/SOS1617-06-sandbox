5d62575c8bd47096f0755c822ff579d79af05b14
SOS161707md <mateuszjacek95@gmail.com>
2017-02-16 18:34:11 +0000

    Code from last class

--- labs/l03t/index.js
+++ labs/l03t/index.js
@@ -1,35 +1,49 @@
-var obj = new Object();
+var contacts = [{
+    name: "Peter",
+    phone: "615826583"
+}, {
+    name: "Anna",
+    phone: "582947257"
+}];
 
-obj.name = "Ktos";
-obj.phone = 172;
-
-function print(contact) {
-    if (contact)
-        console.log(contact.name + " " + contact.phone);
-    else
-        console.log(this.name + " " + this.phone);
+function print(contact, index, contacts) {
+    console.log((parseInt(index) + 1) + "/" + contacts.length + " " + contact.name + " => " + contact.phone);
 }
 
-obj.setAddress = function (address) {
-    this.address = address;
+function getPhones(contacts) {
+    var array = [];
+    for (var i in contacts)
+        array.push(contacts[i].phone);
+    return array;
 }
 
-obj.getAddress = function () {
-    return this.address;
+function getPhonesMap(contacts) {
+    return contacts.map((contact) => {
+        return contact.phone
+    });
 }
 
-obj.print = print;
-obj.setAddress("Petrażyckiego");
+var phones = getPhones(contacts);
+console.log(phones);
 
-function doSth() {
-    var x, y;
-
-    x = 5;
-    y = 6;
-    return x + y;
+function getContactsByInitial(contacts, letter) {
+    return contacts.map((contact) => {
+            return contact.name;
+        }).filter((name) => {
+            return (name.charAt(0) == letter);
+        });
 }
 
+var names = getContactsByInitial(contacts, "P");
+console.log(names);
 
-console.log("Hello World " + doSth());
-obj.print();
-console.log(obj.getAddress());
+// Testing various methods of iteration
+for (var x = 0; x < contacts.length; x++) {
+    print(contacts[x], x, contacts);
+}
+
+contacts.forEach(print);
+
+for (var i in contacts) {
+    print(contacts[i], i, contacts);
+}
